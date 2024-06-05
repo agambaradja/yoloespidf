2c5b22c654cd352da72efa2fbf1c7f909c5b8490
agambaradja <agambaradja68@gmail.com>
2024-06-05 23:07:27 +0700

    Added delay to task stack

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -23,13 +23,13 @@ void camera_task(void *pvParameter) {
     while (true) {
         if (!camera.capture().isOk()) {
             ESP_LOGE(TAG, "Failed to capture frame: %s", camera.exception.toString().c_str());
-//            vTaskDelay(1000 / portTICK_PERIOD_MS); // Delay to avoid tight loop on failure
+            vTaskDelay(1000 / portTICK_PERIOD_MS); // Delay to avoid tight loop on failure
             continue;
         }
 
         if (!yolo.run().isOk()) {
             ESP_LOGE(TAG, "YOLO inference failed: %s", yolo.exception.toString().c_str());
-//            vTaskDelay(1000 / portTICK_PERIOD_MS); // Delay to avoid tight loop on failure
+            vTaskDelay(1000 / portTICK_PERIOD_MS); // Delay to avoid tight loop on failure
             continue;
         }
 
@@ -59,7 +59,7 @@ void camera_task(void *pvParameter) {
             });
         }
 
-//        vTaskDelay(1000 / portTICK_PERIOD_MS); // Delay between captures
+        vTaskDelay(1000 / portTICK_PERIOD_MS); // Delay between captures
     }
 }
 
